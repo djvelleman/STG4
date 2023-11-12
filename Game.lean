18630beee75e4f7614012bad3842c5f5b8706ba1
Dan Velleman <110697570+djvelleman@users.noreply.github.com>
2023-11-12 10:41:26 -0500

    Initial commit

---
+++ Game.lean
@@ -0,0 +1,20 @@
+import Game.Levels.DemoWorld
+
+-- Here's what we'll put on the title screen
+Title "Hello World Game"
+Introduction
+"
+This text appears on the starting page where one selects the world/level to play.
+You can use markdown.
+"
+
+Info "
+Here you can put additional information about the game. It is accessible
+from the starting through the drop-down menu.
+
+For example: Game version, Credits, Link to Github and Zulip, etc.
+
+Use markdown.
+"
+
+MakeGame
